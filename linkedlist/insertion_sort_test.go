b7d9b9efa9eba1b98102289b44b6f6cdb843b9c5
YngwieWang <35023376@qq.com>
2020-06-29 10:23:45 +0800

    improve 147. Insertion Sort List, make it stable and clear

--- linkedlist/insertion_sort_test.go
+++ linkedlist/insertion_sort_test.go
@@ -5,12 +5,12 @@ import (
 	"testing"
 )
 
+// not stable
 func insertionSortList(head *ListNode) *ListNode {
 	p0 := &ListNode{
 		Val:  0,
 		Next: head,
 	}
-
 	for head != nil && head.Next != nil {
 		curr := head.Next
 		next := curr.Next
@@ -30,22 +30,44 @@ func insertionSortList(head *ListNode) *ListNode {
 	return p0.Next
 }
 
+// stable
+func inser(head *ListNode) *ListNode {
+	pre := &ListNode{0, head}
+	for head != nil && head.Next != nil {
+		for ; head.Next != nil && head.Val <= head.Next.Val; head = head.Next {
+		}
+		if head.Next == nil {
+			break
+		}
+		cur := head.Next
+		next := cur.Next
+		tmp := pre
+		for ; cur.Val > tmp.Next.Val; tmp = tmp.Next {
+		}
+		head.Next = next
+		cur.Next = tmp.Next
+		tmp.Next = cur
+	}
+	return pre.Next
+}
+
 func TestInsertionSortList(t *testing.T) {
 	l := &ListNode{
 		Val: 4,
 		Next: &ListNode{
-			Val: 2,
+			Val: 1,
 			Next: &ListNode{
-				Val: 1,
+				Val: 3,
 				Next: &ListNode{
-					Val:  3,
+					Val:  4,
 					Next: nil,
 				},
 			},
 		},
 	}
-	s := insertionSortList(l)
-	for s != nil{
+	//s := insertionSortList(l)
+	s := inser(l)
+	for s != nil {
 		fmt.Printf("%d ", s.Val)
 		s = s.Next
 	}

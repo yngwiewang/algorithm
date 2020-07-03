3d86f2213864682e0d387b09194c3959b041559f
YngwieWang <35023376@qq.com>
2020-07-03 17:01:07 +0800

    improve 148 use first node as pivot

--- linkedlist/quick_sort_linked_list_test.go
+++ linkedlist/quick_sort_linked_list_test.go
@@ -109,51 +109,123 @@ import (
 //	return pre.Next
 //}
 
-func partitionSort(pre, head, pivot, tail, last *ListNode) {
-	small, big := &ListNode{0, nil}, &ListNode{0, nil}
-	preBig := big
-	pre.Next = small
-	for ; head != tail.Next; head = head.Next {
-		if head == pivot {
-			continue
-		}
-		if head.Val < pivot.Val {
-			small.Next = head
-			small = small.Next
-		} else {
-			big.Next = head
-			big = big.Next
-		}
-	}
-	small.Next, pivot.Next, big.Next = pivot, preBig.Next, last
-	pre.Next = pre.Next.Next
-	for h := pre.Next; h != pivot; h = h.Next {
-		if h.Next.Val < h.Val {
-			p := h.Next
-			partitionSort(pre, pre.Next, p, small, pivot)
-		}
-	}
-	h := pivot
-	for ; h.Val == h.Next.Val; h = h.Next {
-	}
+// 用小于前一个结点的结点作为pivot
 
-	for n := h.Next; n != last && n.Next != nil; n = n.Next {
-		if n.Next.Val < n.Val {
-			p := n.Next
-			partitionSort(h, h.Next, p, big, last)
-		}
-	}
-}
+//func partitionSort(pre, head, pivot, tail, last *ListNode) {
+//	small, big := &ListNode{0, nil}, &ListNode{0, nil}
+//	preBig := big
+//	pre.Next = small
+//	for ; head != tail.Next; head = head.Next {
+//		if head == pivot {
+//			continue
+//		}
+//		if head.Val < pivot.Val {
+//			small.Next = head
+//			small = small.Next
+//		} else {
+//			big.Next = head
+//			big = big.Next
+//		}
+//	}
+//	small.Next, pivot.Next, big.Next = pivot, preBig.Next, last
+//	pre.Next = pre.Next.Next
+//	for h := pre.Next; h != pivot; h = h.Next {
+//		if h.Next.Val < h.Val {
+//			p := h.Next
+//			partitionSort(pre, pre.Next, p, small, pivot)
+//		}
+//	}
+//	h := pivot
+//	for ; h.Val == h.Next.Val; h = h.Next {
+//	}
+//
+//	for n := h.Next; n != last && n.Next != nil; n = n.Next {
+//		if n.Next.Val < n.Val {
+//			p := n.Next
+//			partitionSort(h, h.Next, p, big, last)
+//		}
+//	}
+//}
+//
+//func sortList(head *ListNode) *ListNode {
+//	if head == nil {
+//		return head
+//	}
+//	pre := &ListNode{0, head}
+//	flag := false
+//	for ; head.Next != nil; head = head.Next {
+//		if head.Next.Val < head.Val {
+//			flag = true
+//			break
+//		}
+//	}
+//	if flag == false {
+//		return pre.Next
+//	}
+//	p := head.Next
+//	for ; head.Next != nil; head = head.Next {
+//	}
+//	partitionSort(pre, pre.Next, p, head, nil)
+//
+//	return pre.Next
+//}
+
+// 用头结点作为pivot
+//func partitionSort(pre, head, tail *ListNode) {
+//	if head == tail {
+//		return
+//	}
+//	small, big := &ListNode{0, nil}, &ListNode{0, nil}
+//	preSmall, preBig := small, big
+//	pivot := head
+//	moveSmall, moveBig := false, false
+//	for node := head.Next; node != tail; node = node.Next {
+//		if node.Val < pivot.Val {
+//			small.Next = node
+//			small = small.Next
+//			moveSmall = true
+//		} else {
+//			big.Next = node
+//			big = big.Next
+//			moveBig = true
+//		}
+//	}
+//	if !moveSmall && !moveBig{
+//		return
+//	}
+//	if moveSmall {
+//		small.Next = head
+//		pre.Next = preSmall.Next
+//		partitionSort(pre, pre.Next, pivot)
+//	} else {
+//		pre.Next = head
+//	}
+//	if moveBig {
+//		head.Next, big.Next = preBig.Next, tail
+//		partitionSort(pivot, pivot.Next, tail)
+//	} else {
+//		head.Next = tail
+//	}
+//}
+//
+//func sortList(head *ListNode) *ListNode {
+//	if head == nil || head.Next == nil {
+//		return head
+//	}
+//	pre := &ListNode{0, head}
+//	partitionSort(pre, head, nil)
+//	return pre.Next
+//}
 
 func TestPartitionSort1(t *testing.T) {
-	l := &ListNode{
-		Val: 6,
+	r := &ListNode{
+		Val: 4,
 		Next: &ListNode{
-			3,
+			4,
 			&ListNode{
-				6,
+				1,
 				&ListNode{
-					3,
+					4,
 					&ListNode{
 						5,
 						nil,
@@ -162,39 +234,13 @@ func TestPartitionSort1(t *testing.T) {
 			},
 		},
 	}
-	pre := &ListNode{0, l}
-	flag := false
-	for ; l.Next != nil; l = l.Next {
-		if l.Next.Val < l.Val {
-			flag = true
-			break
-		}
-	}
-	if flag == false {
-		return
-	}
-	p := l.Next
-	for ; l.Next != nil; l = l.Next {
-	}
-	partitionSort(pre, pre.Next, p, l, nil)
-	for ; pre != nil; pre = pre.Next {
-		fmt.Printf("%d ", pre.Val)
+
+	l := sortList(r)
+	for ; l != nil; l = l.Next {
+		fmt.Printf("%d ", l.Val)
 
 	}
 }
-
-//func sortList(head *ListNode) *ListNode {
-//	if head == nil {
-//		return head
-//	}
-//	pre := &ListNode{0, head}
-//	for head.Next != nil {
-//		head = head.Next
-//	}
-//	tail := head
-//	partitionSort(pre.Next, tail, pre, nil)
-//	return pre.Next
-//}
 
 //func TestQuickSortLinkedList(t *testing.T) {
 //	s1 := &ListNode{

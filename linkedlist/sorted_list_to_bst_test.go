6be31799dd0e20d0df626cd87ed3c2d485410b60
YngwieWang <35023376@qq.com>
2020-07-07 10:12:19 +0800

    another method 109. add inorder traversal

--- linkedlist/sorted_list_to_bst_test.go
+++ linkedlist/sorted_list_to_bst_test.go
@@ -34,8 +34,60 @@ func sortedListToBST(head *ListNode) *TreeNode {
 	return root
 }
 
+var head *ListNode
+
+func convert(l, r int) *TreeNode {
+	if l > r {
+		return nil
+	}
+	mid := (l + r) / 2
+	left := convert(l, mid-1)
+	node := &TreeNode{head.Val, nil, nil}
+	node.Left = left
+	head = head.Next
+	node.Right = convert(mid+1, r)
+	return node
+}
+
+func sortedListToBST1(head *ListNode) *TreeNode {
+	c := 0
+	for n := head; n != nil; n = n.Next {
+		c++
+	}
+	return convert(0, c-1)
+}
+
 func TestSortedListToBST(t *testing.T) {
 	a := arrayToLinkedList([]int{1, 2, 3, 4, 5})
 	b := sortedListToBST(a)
 	fmt.Println(b)
 }
+
+// inorder traversal, slower
+
+//var head *ListNode
+//
+//func sortedListToBST(h *ListNode) *TreeNode {
+//	var s int
+//	head = h
+//	for h != nil {
+//		s++
+//		h = h.Next
+//	}
+//	return convertListToBST(0, s-1)
+//}
+//
+//func convertListToBST(l, r int) *TreeNode {
+//	if l > r {
+//		return nil
+//	}
+//	mid := (l + r) / 2
+//	left := convertListToBST(l, mid-1)
+//	node := &TreeNode{
+//		Val: head.Val,
+//	}
+//	node.Left = left
+//	head = head.Next
+//	node.Right = convertListToBST(mid+1, r)
+//	return node
+//}

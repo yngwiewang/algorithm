6c7545839d0f9beb0eb01bf6142e3f717951b128
YngwieWang <35023376@qq.com>
2020-07-12 08:19:18 +0800

    add 94. Binary Tree Inorder Traversal, loop method

---
+++ tree/binary_tree_inorder_traversal_test.go
@@ -0,0 +1,24 @@
+package tree
+
+// 94. Binary Tree Inorder Traversal
+
+// loop
+func inorderTraversal(root *TreeNode) []int {
+	var res []int
+	if root == nil {
+		return res
+	}
+	stack := []*TreeNode{}
+	for root != nil || len(stack) > 0 {
+		for ; root != nil; root = root.Left {
+			stack = append(stack, root)
+		}
+		node := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		res = append(res, node.Val)
+		if node.Right != nil {
+			root = node.Right
+		}
+	}
+	return res
+}

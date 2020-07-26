c7b0998b5ea5863de4e093f656e1acbbbab1c429
YngwieWang <35023376@qq.com>
2020-07-26 10:41:11 +0800

    improve 98. min mem

--- tree/validate_binary_search_tree_test.go
+++ tree/validate_binary_search_tree_test.go
@@ -67,7 +67,44 @@ func isValidBSTHelper(root *TreeNode, low, higt int) bool {
 		isValidBSTHelper(root.Right, root.Val, higt)
 }
 
-func TestisValidBSTHelper(t *testing.T) {
-	tree := arrayToBinaryTree([]int{1, 1})
-	t.Log(isValidBSTHelper(tree, math.MinInt32, math.MaxInt32))
+// recursive divide and conquer
+func isValidBST3(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	_, _, ok := isValidBSTHelper1(root)
+	return ok
+}
+
+func isValidBSTHelper1(root *TreeNode) (l, r int, res bool) {
+	res = true
+	if root.Left == nil && root.Right == nil {
+		return root.Val, root.Val, true
+	}
+	lMin, lMax, left := math.MinInt64, math.MinInt64, true
+	rMin, rMax, right := math.MaxInt64, math.MaxInt64, true
+	if root.Left != nil {
+		lMin, lMax, left = isValidBSTHelper1(root.Left)
+	}
+	m := root.Val
+	if root.Right != nil {
+		rMin, rMax, right = isValidBSTHelper1(root.Right)
+	}
+
+	if lMax >= m || m >= rMin {
+		res = false
+	} else {
+		if root.Left == nil {
+			l = m
+		} else {
+			l = lMin
+		}
+		if root.Right == nil {
+			r = m
+		} else {
+			r = rMax
+		}
+	}
+	res = res && left && right
+	return
 }

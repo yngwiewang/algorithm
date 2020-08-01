27a0f091bf39dd8464f9519465a70048f41553fa
yngwiewang <35023376@qq.com>
2020-08-01 23:17:02 +0800

    add 141 test

--- linkedlist/linkedListCycle_test.go
+++ linkedlist/linkedListCycle_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 )
 
+// 141. Linked List Cycle
 func hasCycle(head *ListNode) bool {
 	if head == nil {
 		return false
@@ -23,24 +24,4 @@ func TestHasCycle(t *testing.T) {
 	n := &ListNode{1, nil}
 	n.Next = n
 	t.Log(hasCycle(n))
-}
-
-func Test_hasCycle(t *testing.T) {
-	type args struct {
-		head *ListNode
-	}
-	tests := []struct {
-		name string
-		args args
-		want bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := hasCycle(tt.args.head); got != tt.want {
-				t.Errorf("hasCycle() = %v, want %v", got, tt.want)
-			}
-		})
-	}
 }

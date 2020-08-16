f6a01535fa10bd4b35ec38f19a2af24efc9a7cb3
YngwieWang <35023376@qq.com>
2020-08-16 10:59:24 +0800

    improve 239. Sliding Window Maximum

--- sliding_window/sliding_window_maximum_test.go
+++ sliding_window/sliding_window_maximum_test.go
@@ -9,7 +9,9 @@ import (
 
 // 窗口保存索引
 func maxSlidingWindow1(nums []int, k int) []int {
-	res := make([]int, (len(nums)+1)-k)
+	// res := make([]int, (len(nums)+1)-k)
+	// win := make([]int, 0, k)
+	var res []int
 	var win []int
 	for i, v := range nums {
 		if i >= k && win[0] == i-k {
@@ -20,7 +22,8 @@ func maxSlidingWindow1(nums []int, k int) []int {
 		}
 		win = append(win, i)
 		if i+1 >= k {
-			res[i-k+1] = nums[win[0]]
+			// res[i-k+1] = nums[win[0]]
+			res = append(res, nums[win[0]])
 		}
 	}
 	return res
@@ -29,9 +32,9 @@ func maxSlidingWindow1(nums []int, k int) []int {
 // 窗口保存值
 func maxSlidingWindow(nums []int, k int) []int {
 	res := make([]int, (len(nums)+1)-k)
+	win := make([]int, 0, k)
 	// var res []int
 	// var win  []int
-	win := make([]int, 0, k)
 	for i, v := range nums {
 
 		for len(win) > 0 && win[len(win)-1] < v {
@@ -53,7 +56,7 @@ func Benchmark_maxSlidingWindow(b *testing.B) {
 	a := []int{9, 10, 9, -7, -4, -8, 2, -6}
 	
 	for i := 0; i < b.N; i++ {
-		_ = maxSlidingWindow(a, 5)
+		_ = maxSlidingWindow1(a, 5)
 	}
 }
 
@@ -91,19 +94,18 @@ func Test_maxSlidingWindow(t *testing.T) {
 		args args
 		want []int
 	}{
-		{"3", args{[]int{7, 2, 4}, 2}, []int{7, 4}},
+		{"1", args{[]int{7, 2, 4}, 2}, []int{7, 4}},
 		{"2", args{[]int{1, 3, 1, 2, 0, 5}, 3}, []int{3, 3, 2, 5}},
-		{"1", args{[]int{9, 10, 9, -7, -4, -8, 2, -6}, 5}, []int{10, 10, 9, 2}},
-		{"1", args{[]int{-7, -8, 7, 5, 7, 1, 6, 0}, 4}, []int{7, 7, 7, 7, 7}},
-
-		{"4", args{[]int{1, 3, -1, -3, 5, 3, 6, 7}, 3}, []int{3, 3, 5, 5, 6, 7}},
-		{"5", args{[]int{1, 3, -1, -3, 5, 3, 6, 7}, 2}, []int{3, 3, -1, 5, 5, 6, 7}},
-		{"6", args{[]int{1, 3, -1, -3, 5, 3, 6, 7}, 4}, []int{3, 5, 5, 6, 7}},
-		{"7", args{[]int{1}, 1}, []int{1}},
+		{"3", args{[]int{9, 10, 9, -7, -4, -8, 2, -6}, 5}, []int{10, 10, 9, 2}},
+		{"4", args{[]int{-7, -8, 7, 5, 7, 1, 6, 0}, 4}, []int{7, 7, 7, 7, 7}},
+		{"5", args{[]int{1, 3, -1, -3, 5, 3, 6, 7}, 3}, []int{3, 3, 5, 5, 6, 7}},
+		{"6", args{[]int{1, 3, -1, -3, 5, 3, 6, 7}, 2}, []int{3, 3, -1, 5, 5, 6, 7}},
+		{"7", args{[]int{1, 3, -1, -3, 5, 3, 6, 7}, 4}, []int{3, 5, 5, 6, 7}},
+		{"8", args{[]int{1}, 1}, []int{1}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := maxSlidingWindow(tt.args.nums, tt.args.k); !reflect.DeepEqual(got, tt.want) {
+			if got := maxSlidingWindow1(tt.args.nums, tt.args.k); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("maxSlidingWindow() = %v, want %v", got, tt.want)
 			}
 		})

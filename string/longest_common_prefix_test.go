905d3c1a55f716ca80924427d9a4ec017defb3ee
YngwieWang <35023376@qq.com>
2020-08-17 10:38:05 +0800

    1. 添加小浩算法的方法 2. 优化我的从头遍历累加公共前缀算法，成为各种算法中最快的，没有额外内存消耗 3. 写了个新的从头遍历算法，成为新的最快，没有额外内存分配

--- string/longest_common_prefix_test.go
+++ string/longest_common_prefix_test.go
@@ -1,17 +1,23 @@
 package string
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 // 14. Longest Common Prefix
+
+// 优化方法是不要累加common，而是直接赋值
 func longestCommonPrefix1(strs []string) string {
 	if len(strs) == 0 {
 		return ""
 	}
 	common := ""
-	for i := 0; i < len(strs[0]); i++ {
+	i := 0
+	for ; i < len(strs[0]); i++ {
 		count := 0
 		for _, v := range strs {
-			if len(v) == i{
+			if len(v) == i {
 				return common
 			}
 			if v[i] == strs[0][i] {
@@ -19,7 +25,7 @@ func longestCommonPrefix1(strs []string) string {
 			}
 		}
 		if count == len(strs) {
-			common += string(strs[0][i])
+			common = strs[0][:i+1]
 		} else {
 			return common
 		}
@@ -28,20 +34,20 @@ func longestCommonPrefix1(strs []string) string {
 	return common
 }
 
-func minStr(strs []string) string{
+func minStr(strs []string) string {
 	min := strs[0]
-	for _, v := range strs{
-		if v < min{
+	for _, v := range strs {
+		if v < min {
 			min = v
 		}
 	}
 	return min
 }
 
-func maxStr(strs []string) string{
+func maxStr(strs []string) string {
 	max := strs[0]
-	for _, v := range strs{
-		if v > max{
+	for _, v := range strs {
+		if v > max {
 			max = v
 		}
 	}
@@ -52,16 +58,82 @@ func longestCommonPrefix(strs []string) string {
 	if len(strs) == 0 {
 		return ""
 	}
-	min,max := minStr(strs), maxStr(strs)
-	for i, v := range min{
-		if byte(v) != max[i]{
+	min, max := minStr(strs), maxStr(strs)
+	for i, v := range min {
+		if byte(v) != max[i] {
 			return min[:i]
 		}
 	}
 	return min
 }
 
+// 小浩算法给出的方法，也比较快，不额外消耗内存。
+func longestCommonPrefix2(strs []string) string {
+	if len(strs) == 0 {
+		return ""
+	}
+	common := strs[0]
+	for i := 1; i < len(strs); i++ {
+		if len(strs[i]) == 0 {
+			return ""
+		}
+		for strings.Index(strs[i], common) != 0 {
+
+			common = common[:len(common)-1]
+		}
+
+	}
+	return common
+}
+
+// 这个方法是最快的，也没有额外的内存分配
+func longestCommonPrefix3(strs []string) string {
+	if len(strs) == 0 || len(strs[0]) == 0 {
+		return ""
+	}
+	if len(strs) == 1 {
+		return strs[0]
+	}
+	prefix := strs[0]
+	i := 0
+	for ; i < len(prefix); i++ {
+		for j := 1; j < len(strs); j++ {
+			if i == len(strs[j]) || strs[j][i] != prefix[i] {
+				return prefix[:i]
+			}
+		}
+	}
+	return prefix[:i]
+}
+
 func Test_longestCommonPrefix(t *testing.T) {
+	type args struct {
+		strs []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"0.", args{[]string{"aa", "a"}}, "a"},
+		{"1.", args{[]string{"aa", "aa"}}, "aa"},
+		{"2.", args{[]string{}}, ""},
+		{"3.", args{[]string{""}}, ""},
+		{"4.", args{[]string{"flower", "flow", "flight"}}, "fl"},
+		{"5.", args{[]string{"dog", "dog", "cat"}}, ""},
+		{"6.", args{[]string{"aa", "aa", "aa"}}, "aa"},
+		{"7.", args{[]string{"aa", "", "aa"}}, ""},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := longestCommonPrefix3(tt.args.strs); got != tt.want {
+				t.Errorf("longestCommonPrefix() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Benchmark(b *testing.B) {
 	type args struct {
 		strs []string
 	}
@@ -79,11 +151,9 @@ func Test_longestCommonPrefix(t *testing.T) {
 		{"3.", args{[]string{"aa", "aa", "aa"}}, "aa"},
 		{"4.", args{[]string{"aa", "", "aa"}}, ""},
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := longestCommonPrefix(tt.args.strs); got != tt.want {
-				t.Errorf("longestCommonPrefix() = %v, want %v", got, tt.want)
-			}
-		})
+	for i := 0; i < b.N; i++ {
+		for _, t := range tests {
+			longestCommonPrefix1(t.args.strs)
+		}
 	}
 }

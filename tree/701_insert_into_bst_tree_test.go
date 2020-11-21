63e1bf619cd790fdc2bb7880e6572be4b8e9b910
yngwiewang <35023376@qq.com>
2020-11-21 09:06:00 +0800

    review 701. Insert into a Binary Search Tree

--- tree/701_insert_into_bst_tree_test.go
+++ tree/701_insert_into_bst_tree_test.go
@@ -1,5 +1,8 @@
 package tree
 
+import "testing"
+
+// 701. Insert into a Binary Search Tree
 func insertIntoBST(root *TreeNode, val int) *TreeNode {
 	if root == nil {
 		return &TreeNode{val, nil, nil}
@@ -65,4 +68,52 @@ func insertIntoBST2(root *TreeNode, val int) *TreeNode {
 		root.Left = insertIntoBST2(root.Left, val)
 	}
 	return root
+}
+
+// review 20201121
+func insertIntoBST202011211(root *TreeNode, val int) *TreeNode {
+	newNode := &TreeNode{val, nil, nil}
+	if root == nil {
+		return newNode
+	}
+	n := root
+	for n.Left != nil || n.Right != nil {
+		if val < n.Val {
+			if n.Left == nil {
+				n.Left = newNode
+				return root
+			}
+			n = n.Left
+		} else {
+			if n.Right == nil {
+				n.Right = newNode
+				return root
+			}
+			n = n.Right
+		}
+	}
+	if val < n.Val {
+		n.Left = newNode
+	} else {
+		n.Right = newNode
+	}
+	return root
+}
+
+func insertIntoBST202011212(root *TreeNode, val int) *TreeNode {
+	if root == nil{
+		return &TreeNode{val, nil, nil}
+	}
+	if val < root.Val{
+		root.Left = insertIntoBST202011212(root.Left, val)
+	} else {
+		root.Right = insertIntoBST202011212(root.Right, val)
+	}
+	return root
+}
+func Test_insertBST20201121(t *testing.T) {
+	c := Constructor()
+	tree := c.deserialize("8,#,55,39,#,11,#,#,23,#,#,")
+	instered := insertIntoBST202011212(tree, 17)
+	t.Log(inorderTraversal(instered))
 }

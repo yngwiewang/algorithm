c449009802be6ce6a48eab50d63f4bf4dfe7e68f
yngwiewang <35023376@qq.com>
2020-12-06 09:53:44 +0800

    647. Palindromic Substrings another method

--- dp/647_palindromic_substrings_test.go
+++ dp/647_palindromic_substrings_test.go
@@ -15,7 +15,7 @@ func countSubstrings(s string) int {
 	res := 0
 	for i := len(s) - 1; i >= 0; i-- {
 		for j := i; j < len(s); j++ {
-			if s[i] == s[j] && (j-i <= 1 || dp[i+1][j-1]) {
+			if s[i] == s[j] && (j-i <= 2 || dp[i+1][j-1]) {
 				dp[i][j] = true
 				res++
 			}
@@ -26,6 +26,22 @@ func countSubstrings(s string) int {
 		}
 	}
 	return res
+}
+
+func countSubstrings1(s string) int {
+	res := 0
+	for i := range s {
+		res += countSubstringHelper(s, i, i) + countSubstringHelper(s, i, i+1)
+	}
+	return res
+}
+
+func countSubstringHelper(s string, i, j int) int {
+	count := 0
+	for ; i >= 0 && j < len(s) && s[i] == s[j]; i, j = i-1, j+1 {
+		count++
+	}
+	return count
 }
 
 func Test_countSubstrings(t *testing.T) {
@@ -43,7 +59,7 @@ func Test_countSubstrings(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := countSubstrings(tt.args.s); got != tt.want {
+			if got := countSubstrings1(tt.args.s); got != tt.want {
 				t.Errorf("countSubstrings() = %v, want %v", got, tt.want)
 			}
 		})

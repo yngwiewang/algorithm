c6591edf1160f0caa93c7122cdf89fbc29e0bc09
yngwiewang <35023376@qq.com>
2020-12-12 09:01:02 +0800

    review 516. Longest Palindromic Subsequence

--- dp/516_longest_palindromic_subsequence_test.go
+++ dp/516_longest_palindromic_subsequence_test.go
@@ -65,6 +65,58 @@ func longestPalindromeSubseq1(s string) int {
 	return dp[0][len(s)-1]
 }
 
+func longestPalindromeSubseq20201211(s string) int {
+	memo := make([][]int, len(s))
+	for i := range memo {
+		memo[i] = make([]int, len(s))
+		for j := range memo[i] {
+			memo[i][j] = -1
+		}
+	}
+	res := lpssHelper20201211(s, 0, len(s)-1, memo)
+	fmt.Println(memo)
+	return res
+}
+
+func lpssHelper20201211(s string, i, j int, memo [][]int) int {
+	if i > j {
+		return 0
+	}
+	if i == j {
+		return 1
+	}
+	if memo[i][j] != -1 {
+		return memo[i][j]
+	}
+	if s[i] == s[j] {
+		memo[i][j] = 2 + lpssHelper20201211(s, i+1, j-1, memo)
+		return memo[i][j]
+	}
+	memo[i][j] = common.MaxInt(lpssHelper20201211(s, i+1, j, memo),
+		lpssHelper20201211(s, i, j-1, memo))
+	return memo[i][j]
+}
+
+func longestPalindromeSubseq20201211dp(s string) int {
+	dp := make([][]int, len(s))
+	for i := range dp {
+		dp[i] = make([]int, len(s))
+		dp[i][i] = 1
+	}
+
+	for i := len(s) - 2; i >= 0; i-- {
+		for j := i + 1; j < len(s); j++ {
+			if s[i] == s[j] {
+				dp[i][j] = 2 + dp[i+1][j-1]
+			} else {
+				dp[i][j] = common.MaxInt(dp[i][j-1], dp[i+1][j])
+			}
+		}
+	}
+
+	return dp[0][len(s)-1]
+}
+
 func Test_longestPalindromeSubseq(t *testing.T) {
 	type args struct {
 		s string
@@ -80,7 +132,7 @@ func Test_longestPalindromeSubseq(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := longestPalindromeSubseq(tt.args.s); got != tt.want {
+			if got := longestPalindromeSubseq20201211dp(tt.args.s); got != tt.want {
 				t.Errorf("longestPalindromeSubseq() = %v, want %v", got, tt.want)
 			}
 		})

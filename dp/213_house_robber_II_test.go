4723e3d836aee8c597161ae52c216cf837f7e78b
yngwiewang <35023376@qq.com>
2020-12-20 09:45:14 +0800

    add test case 213. House Robber II

--- dp/213_house_robber_II_test.go
+++ dp/213_house_robber_II_test.go
@@ -9,7 +9,7 @@ import (
 // 213. House Robber II
 
 func robCircle(nums []int) int {
-	if len(nums) == 1{
+	if len(nums) == 1 {
 		return nums[0]
 	}
 	return common.MaxInt(rob(nums[:len(nums)-1]), rob(nums[1:]))
@@ -24,10 +24,10 @@ func Test_robCircle(t *testing.T) {
 		args args
 		want int
 	}{
-		{"1,2", args{[]int{1,2}}, 2},
+		{"1,2", args{[]int{1, 2}}, 2},
 		{"1", args{[]int{1}}, 1},
 		{"2,3,2", args{[]int{2, 3, 2}}, 3},
-		{"12,3,1", args{[]int{1,2,3,1}}, 4},
+		{"1,2,3,1", args{[]int{1, 2, 3, 1}}, 4},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {

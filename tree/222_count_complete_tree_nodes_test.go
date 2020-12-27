edb9c2bbfeca03fdab19100beb4545d2d236608e
yngwiewang <35023376@qq.com>
2020-12-27 09:18:32 +0800

    add 222. Count Complete Tree Nodes

---
+++ tree/222_count_complete_tree_nodes_test.go
@@ -0,0 +1,32 @@
+package tree
+
+// 222. Count Complete Tree Nodes
+
+func countNodes(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return 1 + countNodes(root.Left) + countNodes(root.Right)
+}
+
+func countNodes11(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	res := 0
+	q := []*TreeNode{root}
+	for len(q) > 0 {
+		size := len(q)
+		res += size
+		for _, v := range q {
+			if v.Left != nil {
+				q = append(q, v.Left)
+			}
+			if v.Right != nil {
+				q = append(q, v.Right)
+			}
+		}
+		q = q[size:]
+	}
+	return res
+}

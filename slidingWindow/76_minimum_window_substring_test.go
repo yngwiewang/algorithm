a4e000083debf177cfe84fb2de94c40e1b5ca213
yngwiewang <35023376@qq.com>
2020-12-30 17:28:31 +0800

    add best answer 76. Minimum Window Substring

--- slidingWindow/76_minimum_window_substring_test.go
+++ slidingWindow/76_minimum_window_substring_test.go
@@ -1,6 +1,7 @@
 package slidingwindow
 
 import (
+	"fmt"
 	"math"
 	"testing"
 )
@@ -67,7 +68,7 @@ func minWindow1(s string, t string) string {
 }
 
 // improved a little
-func minWindow(s string, t string) string {
+func minWindow2(s string, t string) string {
 	need, window := make(map[byte]int, len(t)), make(map[byte]int, len(t))
 	minLen := math.MaxInt32
 	res := ""
@@ -115,6 +116,74 @@ func minWindow(s string, t string) string {
 	return res
 }
 
+func minWindow3(s string, t string) string {
+	need, window := make(map[byte]int, len(t)), make(map[byte]int, len(t))
+	minLen := math.MaxInt32
+	res := ""
+	for _, v := range t {
+		need[byte(v)]++
+		window[byte(v)] = 0
+	}
+	l, r := 0, 0
+	valid := 0
+	for r < len(s) {
+		c := s[r]
+		r++
+		if _, ok := need[c]; ok {
+			window[c]++
+			if window[c] == need[c] {
+				valid++
+			}
+		}
+		for valid == len(need) {
+			if r-l < minLen {
+				minLen = r - l
+				res = s[l:r]
+			}
+			d := s[l]
+			l++
+			if _, ok := need[d]; ok {
+				if window[d] == need[d] {
+					valid--
+				}
+				window[d]--
+			}
+		}
+	}
+	return res
+}
+
+// best answer
+func minWindow(s string, t string) string {
+	m := make([]int, 128)
+	minLen := math.MaxInt32
+	res := ""
+	for _, v := range t {
+		m[v]++
+	}
+	fmt.Println(m)
+	l, r := 0, 0
+	valid := len(t)
+	for r < len(s) {
+		if m[s[r]] > 0 {
+			valid--
+		}
+		m[s[r]]--
+		r++
+		for valid == 0 {
+			if r-l < minLen {
+				minLen = r - l
+				res = s[l:r]
+			}
+			m[s[l]]++
+			if m[s[l]] > 0 {
+				valid++
+			}
+			l++
+		}
+	}
+	return res
+}
 func Test_minWindow(t *testing.T) {
 	type args struct {
 		s string
@@ -125,16 +194,17 @@ func Test_minWindow(t *testing.T) {
 		args args
 		want string
 	}{
+		{"1", args{"abca", "ac"}, "ca"},
+		{"1", args{"abc", "ac"}, "abc"},
+		{"1", args{"cabwefgewcwaefgcf", "cae"}, "cwae"},
+		{"1", args{"a", "a"}, "a"},
 		{"1", args{"acbdbaab", "aabd"}, "dbaa"},
 		{"1", args{"ADOBECODEBANC", "ABC"}, "BANC"},
-		{"1", args{"a", "a"}, "a"},
 		{"1", args{"a", "k"}, ""},
 		{"1", args{"abdsfsdfcab", "adk"}, ""},
 		{"1", args{"abdsfsdfcab", "ac"}, "ca"},
 		{"1", args{"abdsfsdfcab", "ac"}, "ca"},
 		{"1", args{"abdsfsdfca", "ac"}, "ca"},
-		{"1", args{"abca", "ac"}, "ca"},
-		{"1", args{"abc", "ac"}, "abc"},
 		{"1", args{"ad", "ad"}, "ad"},
 	}
 	for _, tt := range tests {
